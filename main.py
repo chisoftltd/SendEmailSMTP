4771b900f91afb44d713c10ea88b5cb5e0776c42
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-05 15:39:00 +0000

    Email SMTP with Python

---
+++ main.py
@@ -0,0 +1,13 @@
+import smtplib
+
+my_email = 'python.chinwe@gmail.com'
+pwd = 'pyt@#chinwe'
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=pwd)
+connection.sendmail(from_addr=my_email,
+                    to_addrs='gnexplore@gmail.com',
+                    msg="Subject:Hello\n\n"
+                    "I am testing sending email with python")
+connection.close()
