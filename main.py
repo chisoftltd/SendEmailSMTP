140688dbf9f2c654e75b8763ddda88607f6eb55c
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-05 15:40:17 +0000

    Using SMTP and 'with'

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import smtplib
 my_email = 'python.chinwe@gmail.com'
 pwd = 'pyt@#chinwe'
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=pwd)
-connection.sendmail(from_addr=my_email,
-                    to_addrs='gnexplore@gmail.com',
-                    msg="Subject:Hello\n\n"
-                    "I am testing sending email with python")
-connection.close()
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user=my_email, password=pwd)
+# connection.sendmail(from_addr=my_email,
+#                     to_addrs='gnexplore@gmail.com',
+#                     msg="Subject:Hello\n\n"
+#                     "I am testing sending email with python")
+# connection.close()
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=pwd)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs='gnexplore@gmail.com',
+                        msg="Subject:Hello\n\n"
+                        "I am testing sending email with python")
+
