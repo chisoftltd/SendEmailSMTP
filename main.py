95f2ca52d6fa032776e3df09e40b6761b21b9f7a
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-05 19:26:24 +0000

    Specifying a port number

--- main.py
+++ main.py
@@ -12,24 +12,24 @@ pwd = 'pyt@#chinwe'
 #                     "I am testing sending email with python")
 # connection.close()
 
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=pwd)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs='gnexplore@gmail.com',
-#                         msg="Subject:Hello\n\n"
-#                         "I am testing sending email with python")
+with smtplib.SMTP("smtp.gmail.com", 587, timeout=30) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=pwd)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs='gnexplore@gmail.com',
+                        msg="Subject:Hello\n\n"
+                        "I am testing sending email with python")
 
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-print(year)
-month = now.month
-print(month)
-weekday = now.weekday()
-print(weekday)
-
-# User datetime object
-date_of_brith = dt.datetime(year=1972, month=11, day=30, hour=19)
-print(date_of_brith)
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# print(year)
+# month = now.month
+# print(month)
+# weekday = now.weekday()
+# print(weekday)
+#
+# # User datetime object
+# date_of_brith = dt.datetime(year=1972, month=11, day=30, hour=19)
+# print(date_of_brith)
